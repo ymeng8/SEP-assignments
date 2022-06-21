8fa1d8e4c6d0be2de7ed2e69c7212022a82477b9
ymeng8 <107888849+ymeng8@users.noreply.github.com>
2022-06-21 16:16:52 -0400

    finish assignment 1

---
+++ assignment1.sql
@@ -0,0 +1,60 @@
+--1.
+SELECT ProductID, Name, Color, ListPrice 
+FROM Production.Product
+
+--2.
+SELECT ProductID, Name, Color, ListPrice 
+FROM Production.Product 
+WHERE ListPrice != 0
+
+--3.
+SELECT ProductID, Name, Color, ListPrice 
+FROM Production.Product 
+WHERE Color IS NOT NULL
+
+--4.
+SELECT ProductID, Name, Color, ListPrice 
+FROM Production.Product 
+WHERE Color IS NOT NULL AND ListPrice > 0
+
+--5.
+SELECT Name + ' ' + Color 
+FROM Production.Product 
+WHERE Color IS NOT NULL
+
+--6.
+SELECT Name, Color 
+FROM Production.Product 
+WHERE Name IN ('LL Crankarm', 'ML Crankarm', 'HL Crankarm', 'Chainring Bolts', 'Chainring Nut', 'Chainring') AND Color IN ('Black','Silver')
+
+--7.
+SELECT ProductID, Name
+FROM Production.Product
+WHERE ProductID BETWEEN 400 AND 500
+
+--8.
+SELECT ProductID, Name, Color
+FROM Production.Product
+WHERE Color IN ('Black','Blue')
+
+--9.
+SELECT Name
+FROM Production.Product
+WHERE Name LIKE 'S%'
+
+--10.
+SELECT Name, ListPrice
+FROM Production.Product
+WHERE Name LIKE 'A%' OR Name LIKE 'S%'
+ORDER BY Name
+
+--11.
+SELECT Name
+FROM Production.Product
+WHERE Name LIKE 'SPO[^K]%'
+ORDER BY Name
+
+--12.
+SELECT DISTINCT ProductSubcategoryID, Color
+FROM Production.Product
+WHERE ProductSubcategoryID IS NOT NULL AND Color IS NOT NULL
